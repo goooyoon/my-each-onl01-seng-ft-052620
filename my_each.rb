348c2a38ac41c57e30344c852a48819a784cbfc0
Grace Kim <goooyoon@gmail.com>
2020-05-29 01:54:14 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,8 @@
-def my_each(arguments)
-
-i = 0
-  while i < array.size
+def my_each(array) 
+i = 0 
+  while i < array.length 
     yield array[i]
-    i += 1
-  end
-  array
+    i += 1 
+  end 
+  array 
 end
